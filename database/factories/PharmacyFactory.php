efb07b7c210004ee3d5f34fb07b88c0047d9bd1b
MohammedSallam <mosallam06@gmail.com>
2020-09-05 21:15:54 +0200

    Edit distance to rania location

--- database/factories/PharmacyFactory.php
+++ database/factories/PharmacyFactory.php
@@ -7,10 +7,12 @@ use Faker\Generator as Faker;
 
 $factory->define(Pharmacy::class, function (Faker $faker) {
     return [
+        // rania location 30.1338824,31.2682503
+        // 30.1338824,31.2682503
         'name' => $faker->sentence,
         'phone' => $faker->e164PhoneNumber,
         'address' => $faker->address,
-        'lat' => $faker->randomFloat(6, 30.778054, 30.782433),
-        'lng' => $faker->randomFloat(6, 30.989893, 30.988500),
+        'lat' => $faker->randomFloat(7, 30.1338824, 31.2682503), // 30.778054,30.782433
+        'lng' => $faker->randomFloat(7, 30.1338824, 31.2682503), //30.989893,30.988500
     ];
 });
